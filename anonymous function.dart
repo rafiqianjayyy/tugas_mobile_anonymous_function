0c42842fc6c0130d4edc24ac344064591ace9523
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-12 22:46:03 +0700

    Create anonymous function.dart

---
+++ anonymous function.dart
@@ -0,0 +1,28 @@
+//INI ADALAH CONTOH ANONYMOUS FUNCTION AS VARIABEL
+// void main(){
+
+//   var fungsikapital = (String nama){
+//     return nama.toUpperCase();
+//   };
+
+//   var fungsikecil = (String nama) => nama.toLowerCase();
+
+//   var hasil1 = fungsikapital('Rafiqi');
+//   print(hasil1);
+
+//   var hasil2 = fungsikecil('rafiqi');
+//   print(hasil2);
+// }
+
+//INI ADALAH CONTOH ANONYMOUS FUNCTION AS PARAMETER
+// void halo(String nama, String Function(String) filter){
+//   print('hai ${filter(nama)}');
+// }
+
+// void main(){
+//   halo('Muhammad Rafiqi', (nama){
+//     return nama.toUpperCase();
+//   });
+
+//   halo('Muhammad Rafiqi', (nama)=> nama.toLowerCase());
+// }
